3f65ef96eb802c730c17ea567d5c23361a54d70a
Abhilekh0724 <nimwang0724@gmail.com>
2024-07-27 11:15:11 +0545

    pic

---
+++ controllers/searchController.js
@@ -0,0 +1,38 @@
+const Category = require('../models/adminModels'); // Ensure the correct path
+const path = require('path');
+const fs = require('fs');
+
+const searchCategory = async (req, res) => {
+  const searchQuery = req.query.q || "";
+  const searchCategory = req.query.tags || "";
+
+  try {
+    const filter = {};
+
+    if (searchQuery) {
+      filter.name = { $regex: searchQuery, $options: "i" }; // Search by category name
+    }
+
+    if (searchCategory) {
+      filter.info = { $regex: searchCategory, $options: "i" }; // Search by category info
+    }
+
+    // Find categories
+    const categories = await Category.find(filter);
+    res.status(200).json({
+      success: true,
+      message: "Categories Fetched!",
+      categories: categories,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Server Error!",
+    });
+  }
+};
+
+module.exports = {
+  searchCategory,
+};
