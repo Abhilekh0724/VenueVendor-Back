050fbdc3a6d531d2066f2710d75a6f807aa84cf8
Abhilekh0724 <nimwang0724@gmail.com>
2024-07-29 16:31:34 +0545

    admin

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const adminControllers = require('../controllers/adminControllers');
 const singleControllers = require('../controllers/singleControllers');
 const searchControllers = require('../controllers/searchControllers');
+const { authGuard, adminGuard } = require("../middleware/authGuard");
 
 // Admin routes
 router.post('/create', adminControllers.createCategory);
