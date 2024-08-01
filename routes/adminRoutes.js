534b335469e4d67884d145e133deb90c9b236b5d
Abhilekh0724 <nimwang0724@gmail.com>
2024-08-01 13:41:30 +0545

    auth guard

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const adminControllers = require('../controllers/adminControllers');
 const singleControllers = require('../controllers/singleControllers');
 const searchControllers = require('../controllers/searchControllers');
-const { authGuard, adminGuard } = require("../middleware/authGuard");
+// const { authGuard, adminGuard } = require("../middleware/authGuard");
 
 // Admin routes
 router.post('/create', adminControllers.createCategory);
