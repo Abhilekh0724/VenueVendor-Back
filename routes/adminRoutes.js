e42118d21c85a0db1afa8e556826361c023db70d
Abhilekh0724 <nimwang0724@gmail.com>
2024-08-09 21:05:21 +0545

    auth

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 const adminControllers = require('../controllers/adminControllers');
 const singleControllers = require('../controllers/singleControllers');
 const searchControllers = require('../controllers/searchControllers');
-// const { authGuard, adminGuard } = require("../middleware/authGuard");
+const { authGuard, adminGuard } = require("../middleware/authGuard");
 
 // Admin routes
-router.post('/create', adminControllers.createCategory);
+router.post('/create',authGuard, adminGuard, adminControllers.createCategory);
 router.get('/get', adminControllers.getAllCategories);
-router.put('/update/:id', adminControllers.updateCategory); // Update route
-router.delete('/delete/:id', adminControllers.deleteCategory); // Delete route
+router.put('/update/:id',authGuard, adminGuard, adminControllers.updateCategory); // Update route
+router.delete('/delete/:id',authGuard, adminGuard, adminControllers.deleteCategory); // Delete route
 router.get('/search', searchControllers.searchCategory);
 
 // Single category route
--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { uploadProfilePic } = require('../controllers/profileControllers');
+const { authGuard } = require('../middleware/authGuard'); // Import the authGuard middleware
 const router = express.Router();
 
-router.post('/uploadProfilePic', uploadProfilePic);
+// Apply authGuard middleware to ensure only authenticated users can access this route
+router.post('/uploadProfilePic', authGuard, uploadProfilePic);
 
 module.exports = router;
